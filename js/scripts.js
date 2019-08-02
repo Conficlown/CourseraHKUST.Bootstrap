3b13c5f4ce9ce716b1e551169b0f77656dc3fdb1
Conficlown <wwstargazer@hotmail.com>
2019-08-03 00:20:31 +0200

    finished assignments

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    // $("#mycarousel").carousel( { interval: 2000 } );
+    // $("#carousel-pause").click(function(){
+    //     $("#mycarousel").carousel('pause');
+    // });
+    // $("#carousel-play").click(function(){
+    //     $("#mycarousel").carousel('cycle');
+    // });
+
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('toggle');
+    }).hover(function(){
+        $("#loginButton").css({'cursor': 'pointer'});
+    });
+
+    $("#reserveTableButton").click(function(){
+        $("#reserveModal").modal('toggle');
+    }).hover(function(){
+        $("#reserveTableButton").css({'cursor': 'pointer'});
+    });
+});
